9afffe6ff65ea11f62fc95dcf0b1585dcc2f6d06
Erik Kronquist <erik.kronquist@ucdenver.edu>
2025-09-19 20:58:26 -0600

    End lecture 158

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -8,6 +8,24 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return 
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            category.color.withAlpha(140),
+            category.color.withAlpha(230),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+          color: Theme.of(context).colorScheme.onSurface,
+        ),
+      ),
+    );
   }
 }
