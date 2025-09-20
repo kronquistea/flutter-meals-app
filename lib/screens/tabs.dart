05634a9518d1a7f3a5a6138f3a4c69362eafdf17
Erik Kronquist <erik.kronquist@ucdenver.edu>
2025-09-20 17:27:48 -0600

    End lecture 178

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -49,15 +49,17 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
-  void _setScreen(String identifier) {
+  void _setScreen(String identifier) async {
     Navigator.pop(context);
     if (identifier == 'filters') {
       // could use pushReplacement to disable "back button"
-      Navigator.of(context).push(
+      final result = await Navigator.of(context).push<Map<Filter, bool>>(
         MaterialPageRoute(
           builder: (ctx) => const FiltersScreen(),
         ),
       );
+
+      print(result);
     }
   }
 
