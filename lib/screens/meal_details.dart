e82e4d3cfb8c5c116fbfcf5a21c218b1dcbb44e8
Erik Kronquist <erik.kronquist@gmail.com>
2025-09-26 11:37:31 -0600

    End lecture 204

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -42,11 +42,17 @@ class MealDetailsScreen extends ConsumerWidget {
               duration: const Duration(milliseconds: 300),
               transitionBuilder: (child, animation) {
                 return RotationTransition(
-                  turns: animation,
+                  turns: Tween<double>(
+                    begin: 0.95,
+                    end: 1,
+                  ).animate(animation),
                   child: child,
                 );
               },
-              child: Icon(isFavorite ? Icons.star : Icons.star_border),
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
             ),
           ),
         ],
